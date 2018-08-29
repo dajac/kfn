1f3d64508a164586acb5c1d1f034d37797db5f39
David Jacot <david.jacot@gmail.com>
2018-08-29 17:53:52 +0200

    Add release script

--- hack/release.bash
+++ hack/release.bash
@@ -3,10 +3,12 @@
 ROOT=${ROOT:-$(git rev-parse --show-toplevel)}
 
 ORG=dajac
+REPO=kfn
 NAME=kfn-operator
 VERSION=$(git describe --tags --dirty)
-COMMIT_HASH=$(git rev-parse --short HEAD 2>/dev/null)
-DATE=$(date "+%Y-%m-%d")
+#COMMIT_HASH=$(git rev-parse --short HEAD 2>/dev/null)
+#DATE=$(date "+%Y-%m-%d")
+TARGET_MANIFEST=${ROOT}/config/kfn-${VERSION}.yaml
 
 if [[ "$(pwd)" != "${ROOT}" ]]; then
   echo "you are not in the root of the repo" 1>&2
@@ -16,3 +18,40 @@ fi
 
 docker build -t ${ORG}/${NAME}:${VERSION} .
 docker push ${ORG}/${NAME}:${VERSION}
+
+rm ${TARGET_MANIFEST} 2>/dev/null
+
+for file in `ls ${ROOT}/config/`; do
+    cat ${ROOT}/config/$file >> ${TARGET_MANIFEST}
+    echo "---" >> ${TARGET_MANIFEST}
+done;
+
+if [[ -z "$ACCESS_TOKEN" ]]; then
+  echo "Unable to release: Github Token not specified" > /dev/stderr
+  exit 1
+fi
+
+payload=$(
+  jq --null-input \
+     --arg tag "$VERSION" \
+     --arg name "$VERSION" \
+     '{ tag_name: $tag, name: $name, draft: true }'
+)
+
+response=$(
+  curl --fail \
+       --header "Authorization: token $ACCESS_TOKEN" \
+       --silent \
+       --location \
+       --data "$payload" \
+       "https://api.github.com/repos/${ORG}/${REPO}/releases"
+)
+
+upload_url="$(echo "$response" | jq -r .upload_url | sed -e "s/{?name,label}//")"
+
+curl --header "Authorization: token $ACCESS_TOKEN" \
+     --header "Content-Type:text/yaml" \
+     --data-binary "@${TARGET_MANIFEST}" \
+     "$upload_url?name=$(basename "${TARGET_MANIFEST}")"
+
+rm ${TARGET_MANIFEST} 2>/dev/null
